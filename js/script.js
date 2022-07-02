c92b2882f1239a8be1e1e3263487533277189fb9
soaringlee <leeyuanpeng@hotmail.com>
2022-07-02 21:45:12 +0800

    Site updated: 2022-07-02 21:45:11

--- js/script.js
+++ js/script.js
@@ -804,20 +804,3 @@ function copy (text) {
     }
     return isSuccess
 }
-
-var options = {
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-import Darkmode from 'darkmode-js';
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
